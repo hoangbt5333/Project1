ee71a69471bb78b02e4343fb1aea3f5eb601d8dc
hoangbt5333 <hoangvbhp2005@gmail.com>
2025-10-22 14:51:28 +0700

    hoan thanh chuc nang CRUD lop

--- routes/lop.js
+++ routes/lop.js
@@ -7,6 +7,7 @@ function isLoggedIn(req, res, next) {
   res.redirect('/user/login');
 }
 
+// Hiển thị danh sách lớp
 router.get('/', isLoggedIn, (req, res) => {
   const sql = `
     SELECT * FROM lop
@@ -18,6 +19,98 @@ router.get('/', isLoggedIn, (req, res) => {
       return res.status(500).send('Error querying database');
     }
     res.render('lop', { title: 'Quản lý lớp', classes: results });
+  });
+});
+
+// Hiển thị form thêm lớp
+router.get('/add', isLoggedIn, (req, res) => {
+  const sqlKhoa = `
+    SELECT * FROM khoa
+  `;
+
+  db.query(sqlKhoa, (err, khoaList) => {
+    if (err) {
+      // console.error('❌ Lỗi truy vấn SQL:', err);
+      return res.status(500).send('Error querying database');
+    }
+    res.render('lop_add', { title: 'Thêm lớp', khoaList: khoaList });
+  });
+});
+
+// Thêm lớp mới
+router.post('/add', isLoggedIn, (req, res) => {
+  const { ma_lop, ten_lop, ma_khoa } = req.body;
+
+  const sql = `
+    INSERT INTO lop (ma_lop, ten_lop, ma_khoa)
+    VALUES (?, ?, ?)
+  `;
+
+  db.query(sql, [ma_lop, ten_lop, ma_khoa], (err, results) => {
+    if (err) {
+      // console.error('❌ Lỗi truy vấn SQL:', err);
+      return res.status(500).send('Error querying database');
+    }
+    res.redirect('/lop');
+  });
+});
+
+// Hiển thị form sửa lớp
+router.get('/edit/:ma_lop', isLoggedIn, (req, res) => {
+  const ma_lop = req.params.ma_lop;
+
+  const sqlLop = `
+    SELECT * FROM lop WHERE ma_lop = ?
+  `;
+  const sqlKhoa = `
+    SELECT * FROM khoa
+  `;
+
+  db.query(sqlLop, [ma_lop], (err, results) => {
+    if (err) {
+      // console.error('❌ Lỗi truy vấn SQL:', err);
+      return res.status(500).send('Error querying database');
+    }
+    if (results.length === 0) {
+      return res.status(404).send('Lớp học không tồn tại');
+    }
+
+    db.query(sqlKhoa, (err, khoaList) => {
+      if (err) {
+        // console.error('❌ Lỗi truy vấn SQL:', err);
+        return res.status(500).send('Error querying database');
+      }
+      res.render('lop_edit', { title: 'Sửa lớp', classes: results[0], khoaList: khoaList });
+    });
+  });
+});
+// Cập nhật thông tin lớp
+router.post('/edit/:ma_lop', isLoggedIn, (req, res) => {
+  const ma_lop = req.params.ma_lop;
+  const { ten_lop, ma_khoa } = req.body;
+  const sql = `
+    UPDATE lop SET ten_lop = ?, ma_khoa = ? WHERE ma_lop = ?
+  `;
+  db.query(sql, [ten_lop, ma_khoa, ma_lop], (err, results) => {
+    if (err) {
+      // console.error('❌ Lỗi truy vấn SQL:', err);
+      return res.status(500).send('Error querying database');
+    }
+    res.redirect('/lop');
+  });
+});
+// Xóa lớp
+router.get('/delete/:ma_lop', isLoggedIn, (req, res) => {
+  const ma_lop = req.params.ma_lop;
+  const sql = `
+    DELETE FROM lop WHERE ma_lop = ?
+  `;
+  db.query(sql, [ma_lop], (err, results) => {
+    if (err) {
+      // console.error('❌ Lỗi truy vấn SQL:', err);
+      return res.status(500).send('Error querying database');
+    }
+    res.redirect('/lop');
   });
 });
 
