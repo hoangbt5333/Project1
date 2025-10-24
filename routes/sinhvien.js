d190a29bf93e5cb43d738d9a331a8f46c92db574
hoangbt5333 <hoangvbhp2005@gmail.com>
2025-10-24 20:20:22 +0700

    co dinh layout, chinh sua giao dien

--- routes/sinhvien.js
+++ routes/sinhvien.js
@@ -7,6 +7,7 @@ function isLoggedIn(req, res, next) {
   res.redirect('/user/login');
 }
 
+// Hiển thị danh sách sinh viên
 router.get('/', isLoggedIn, (req, res) => {
   const sql = `
     SELECT sv.stt, sv.ma_sv, sv.ho_ten, l.ten_lop
@@ -21,6 +22,103 @@ router.get('/', isLoggedIn, (req, res) => {
       return res.status(500).send('Error querying database');
     }
     res.render('sinhvien', { title: 'Quản lý sinh viên', students: results });
+  });
+});
+
+// Hiển thị form thêm sinh viên
+router.get('/add', isLoggedIn, (req, res) => {
+  const sqlLop = `
+    SELECT * FROM lop
+  `;
+
+  db.query(sqlLop, (err, lopList) => {
+    if (err) {
+      // console.error('❌ Lỗi truy vấn SQL:', err);
+      return res.status(500).send('Error querying database');
+    }
+    res.render('sinhvien_add', { title: 'Thêm sinh viên', lopList: lopList });
+  });
+});
+
+// Thêm sinh viên mới
+router.post('/add', isLoggedIn, (req, res) => {
+  const { ma_sv, ho_ten, ma_lop } = req.body;
+
+  const sqlInsert = `
+    INSERT INTO sinhvien (ma_sv, ho_ten, ma_lop)
+    VALUES (?, ?, ?)
+  `;
+
+  db.query(sqlInsert, [ma_sv, ho_ten, ma_lop], (err, result) => {
+    if (err) {
+      // console.error('❌ Lỗi truy vấn SQL:', err);
+      return res.status(500).send('Error inserting data');
+    }
+    res.redirect('/sinhvien');
+  });
+});
+
+// Hiển thị form sửa sinh viên
+router.get('/edit/:ma_sv', isLoggedIn, (req, res) => {
+  const ma_sv = req.params.ma_sv;
+
+  const sqlSV = `
+    SELECT * FROM sinhvien WHERE ma_sv = ?
+  `;
+
+  const sqlLop = `
+    SELECT * FROM lop
+  `
+
+  db.query(sqlSV, [ma_sv], (err, results) => {
+    if (err) {
+      // console.error('❌ Lỗi truy vấn SQL:', err);
+      return res.status(500).send('Error querying database');
+    }
+    if (results.length === 0) {
+      return res.status(404).send('Sinh viên không tồn tại');
+    }
+    db.query(sqlLop, (err, lopList) => {
+      if (err) {
+        // console.error('❌ Lỗi truy vấn SQL:', err);
+        return res.status(500).send('Error querying database');
+      }
+      res.render('sinhvien_edit', { title: 'Sửa sinh viên', students: results[0], lopList: lopList });
+    });
+  });
+});
+// Cập nhật thông tin sinh viên
+router.post('/edit/:ma_sv', isLoggedIn, (req, res) => {
+  const ma_sv = req.params.ma_sv;
+  const { ma_sinh_vien, ho_ten, ma_lop } = req.body;
+  const sql = `
+    UPDATE sinhvien
+    SET ma_sv = ?, ho_ten = ?, ma_lop = ?
+    WHERE ma_sv = ?
+  `;
+  db.query(sql, [ma_sinh_vien, ho_ten, ma_lop, ma_sv], (err, result) => {
+    if (err) {
+      // console.error('❌ Lỗi truy vấn SQL:', err);
+      return res.status(500).send('Error updating data');
+    }
+    res.redirect('/sinhvien');
+  });
+});
+
+// Xóa sinh viên
+router.post('/delete/:ma_sv', isLoggedIn, (req, res) => {
+  const ma_sv = req.params.ma_sv;
+
+  const sql = `
+    DELETE FROM sinhvien
+    WHERE ma_sv = ?
+  `;
+  db.query(sql, [ma_sv], (err, result) => {
+    if (err) {
+      // console.error('❌ Lỗi truy vấn SQL:', err);
+      return res.status(500).send('Error deleting data');
+    }
+    res.redirect('/sinhvien');
   });
 });
 
